21be4297cc1be723c34c94ae19764323bf0847a3
figufigu100 <132096198+figufigu100@users.noreply.github.com>
2023-04-28 22:22:28 +0300

    Create informations.py

---
+++ informations.py
@@ -0,0 +1,8 @@
+import random
+
+#while true tekrar sağlar
+while True:
+    bilgi=["1=1","1+1=2-1","İnsanlar uçar.","Gerçekler gerçektir."]
+    #enter a basmadan geçmez
+    qwe=input()
+    print(random.choice(bilgi))

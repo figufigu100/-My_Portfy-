100d61cc977e8c0d87cc6dbafa5e3286ac9903a9
figufigu100 <132096198+figufigu100@users.noreply.github.com>
2023-08-25 22:16:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
